d4e4d86cf61abfeb2b67569881e9a76906f56b7a
Lucas Nardelli <lucasnardellisaar@gmail.com>
2022-06-01 11:53:21 -0300

    Implementação do algoritmo FCFS

--- codigo-base/algorithms/FCFS.h
+++ codigo-base/algorithms/FCFS.h
@@ -68,7 +68,43 @@ void fcfs_print_gantt_chart(Process *p, int len)
 
 void FCFS(Process *p, int len)
 {
-    printf("Implementar o FCFS");
+    int i;
+    int total_waiting_time = 0;
+    int total_turnaround_time = 0;
+    int total_response_time = 0;
+
+    process_init(p, len);
+    merge_sort_by_arrive_time(p, 0, len);
+
+    for (i = 0; i < len; i++)
+    {
+        if(i==0){
+            p[i].response_time = 0;
+            p[i].waiting_time = 0;
+            p[i].return_time = p[i].burst;
+        } else {
+            p[i].return_time = p[i-1].return_time + p[i].burst;
+            p[i].waiting_time = p[i-1].return_time - p[i].arrive_time;
+            p[i].response_time = p[i-1].return_time - p[i].arrive_time;
+        }
+    }
+
+    for (i = 0; i < len; i++)
+    {
+        p[i].turnaround_time = p[i].return_time - p[i].arrive_time;
+        total_waiting_time += p[i].waiting_time;
+        total_turnaround_time += p[i].turnaround_time;
+        total_response_time += p[i].response_time;
+    }
+
+    printf("\tExemplo dado pelo professor: FCFS \n\n");
+
+    pps_print_gantt_chart(p, len);
+    printf("\n\tAverage Waiting Time     : %-2.2lf\n", (double)total_waiting_time / (double)len);
+    printf("\tAverage Turnaround Time  : %-2.2lf\n", (double)total_turnaround_time / (double)len);
+    printf("\tAverage Response Time    : %-2.2lf\n\n", (double)total_response_time / (double)len);
+
+    print_table(p, len);
 }
 
 #endif
